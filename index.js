2534022bb545c54cc0e6dbc651b31ab497c982a3
fabiosachetto <fabio@sachetto.com.br>
2021-06-06 20:51:37 -0300

    seguindo o fluxo do curso e vendo de novo sobre if e else

--- index.js
+++ index.js
@@ -14,10 +14,24 @@ var photo = document.querySelector('#exampleInputFile');
 
 var admin = document.querySelector('#exampleInputAdmin');
 
+
+
+
 var fields = document.querySelectorAll('#form-user-create [name]');
-fields.forEach(function (field, index){
-    console.log(field.id, field.name, field.value, field.checked, index);
-})
+fields.forEach(function(field, index){
+
+    if (field.name == 'gender') {
+        if (field.checked) {
+            
+            console.log('Sim', field);
+
+        }
+    } else {
+        console.log('Não');
+    }
+
+    //console.log(field.id, field.name, field.value, field.checked, index);
+});
 
 /*var olaMundo = ('Olá Mundo');
 console.log("Primeira Variável " + olaMundo);
