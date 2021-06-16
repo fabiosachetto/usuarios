da9cf65d444ee7e4acd9f59f1443e1e5aded315b
fabiosachetto <fabio@sachetto.com.br>
2021-06-15 21:13:30 -0300

    trabalhando com funções

--- index.js
+++ index.js
@@ -1,162 +1,56 @@
-/*var name = document.querySelector('#exampleInputName');
-
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-
-var birth = document.querySelector('#exampleInputBirth');
-
-var country = document.querySelector('#exampleInputCountry');
-
-var email = document.querySelector('#exampleInputEmail');
-
-var password = document.querySelector('#exampleInputPassword');
-
-var photo = document.querySelector('#exampleInputFile');
-
-var admin = document.querySelector('#exampleInputAdmin');*/
-
-
-
-
 var fields = document.querySelectorAll("#form-user-create [name]");
 
 //Criando um objeto (no caso é um JSon)
 var user = {  };
+
+function addLine(dataUser){
+    
+    //Criando um template string
+    var tr = document.createElement("tr");
+
+    tr.innerHTML = `
+
+        <tr>
+            <td>
+                <img src="dist/img/user1-128x128.png" alt="User Image" class="img-circle img-sm">
+            </td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    
+    `;
+
+    document.getElementById("table-users").appendChild(tr);
+
+}
 
 //getElementById serve para pegar o ID da tag HTML q vai ser manipulado. addEventListener pega o evento q o botão vai realizar.
 document.getElementById("form-user-create").addEventListener("submit", function(event){
 
     event.preventDefault();
 
-    fields.forEach(function(field){
+    fields.forEach(function (field, index){
 
         if (field.name == "gender") {
+
             if (field.checked) {
                 user[field.name] = field.value;
             }
+
         } else {
+
             user[field.name] = field.value;
+
         }
+
     });
-    
-    console.log(user);
+
+    addLine(user);
 
 });
-
-/*var olaMundo = ('Olá Mundo');
-console.log("Primeira Variável " + olaMundo);
-
-
-
-
-let a = 10;
-const b = "10";
-console.log ("let a é igual a const b? " + (a !== b));
-
-
-
-
-let cor = "preto";
-if (cor === "verde") {
-
-    console.log ("A variável cor é igual a verde!");
-
-} else if (cor === "preto") {
-
-    console.log ("A variável cor é diferente de verde!");
-
-} else {
-
-    console.log ("Presta atenção, a variável cor é igual a preto");
-
-}
-
-
-
-
-//Switch
-let corSwitch = "preto";
-
-switch (corSwitch) {
-    case "verde":
-        console.log ("A variável cor é igual a verde!");
-        break;
-
-
-    case "amarelo":
-        console.log ("A variável cor é igual a amarelo!");
-        break;
-
-
-    case "azul":
-        console.log ("A variável cor é igual a azul!");
-        break;
-        
-    default:
-        console.log ("Vc não acertou, a variável cor é preto");
-}
-
-//For
-let numero = 9;
-
-for (let i = 0; i <= 10; i++) {
-                 //Isso é um template string
-    console.log (`${i} X ${numero} = ${i*numero}`);
-}
-
-//Funções
-function calculadora(x1, x2, operador) {
-    return eval(`${x1} ${operador} ${x2}`);
-}
-
-let resultado = calculadora(2, 2, "*");
-
-console.log (resultado);
-
-//Arrow Function
-let calc = (x1, x2, operator) => {
-    return eval(`${x1} ${operator} ${x2}`);
-}
-
-let result = calc(5, 2, "*");
-
-console.log (result);
-
-//Array
-let carros = ["gol", "corsa", 'palio'];
-
-//Laço para percorrer no Array
-carros.forEach(function(value, index){
-
-    console.log ("Quantidade de carros é: ", carros.length, "A posição é: ", index, "O valor é: ", value);
-});
-
-//Classes = Conjunto de Métodos e Atributos
-let celular1 = function(){
-    this.color = "azul";
-
-    this.ligar = function(){
-        console.log ("fazendo uma ligação");
-        return ("ligando");
-    }
-}
-
-let objeto = new celular1();
-
-console.log (objeto.ligar());
-
-//Classes com o ES6
-class celular2 {
-    //Constructor é um método construtor
-    constructor(){
-        this.color = "azul";
-    }
-
-    ligar2() {
-        console.log ("fazendo uma ligação Classes com o ES6");
-        return ("ligando Classes com o ES6");
-    }
-}
-
-let objeto2 = new celular2();
-
-console.log (objeto2.ligar2());*/
